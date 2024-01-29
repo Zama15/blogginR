a08f9ba5bdbd91591e66c199c8ffa79f688a425e
Zama15 <alexavdstiles@gmail.com>
2024-01-29 14:50:43 -0600

    Refactor legacy_authenticate.php
    Previusly password_verify pass throught a if statement to determinate if
    the exit was 0 (without errors) or 1 (with errors). The refactor version
    pass the negative boolean result to a variable and there is changed the data
    type to int and pass to the exit method.
    This changed is thanks to "Your Common Sense" from stack exchange

--- app/scripts/legacy_authenticate.php
+++ app/scripts/legacy_authenticate.php
@@ -1,10 +1,7 @@
 <?php
 $password = $argv[1];
-$hased_password = $argv[2];
+$hashed_password = $argv[2];
 
-if (password_verify($password, $hased_password)) {
-  exit(0);
-} else {
-  exit(1);
-}
+$result = !password_verify($password, $hashed_password);
+exit((int)$result);
 ?>
